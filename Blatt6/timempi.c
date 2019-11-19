bc145198feda9d18b0d66ab7a780f85db804082e
Mytlogos <mytlogos@hotmail.de>
2019-11-19 13:43:05 +0100

    add mpi communcation

--- Blatt6/timempi.c
+++ Blatt6/timempi.c
@@ -4,6 +4,9 @@
 #include <inttypes.h>
 #include <string.h>
 #include <mpi.h>
+#include <unistd.h>
+#include <sys/time.h>
+#include <time.h>
 
 int main(int argc, char** argv) {
     // Initialize the MPI environment
@@ -12,20 +15,44 @@ int main(int argc, char** argv) {
     // Get the number of processes
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
+	
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+		
+	char buffer[30];
+	struct timeval tv;
 
-    // Get the name of the processor
-    char processor_name[MPI_MAX_PROCESSOR_NAME];
-    int name_len;
-    MPI_Get_processor_name(processor_name, &name_len);
+	time_t curtime;
 
-    // Print off a hello world message
-    printf("Hello world from processor %s, rank %d out of %d processors\n",
-           processor_name, world_rank, world_size);
+	gettimeofday(&tv, NULL); 
+	curtime=tv.tv_sec;
 
+	strftime(buffer,30,"%d.%m.%Y %T.",localtime(&curtime));
+	char name[150];
+	memset(name, 0, 150);
+	gethostname(name, 150);
+    
+	char *responses[world_size];
+
+	char message[255];
+	sprintf(message, "%s: %s%ld\n", name, buffer, tv.tv_usec);
+
+	if (world_rank == 0) {
+		for (int i = 1; i < world_size; i++){
+			MPI_Recv(&responses[i], 255, MPI_BYTE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
+	} else {
+		char message[255];
+		sprintf(message, "%s: %s%ld\n", name, buffer, tv.tv_usec);
+		MPI_Send(message, 255, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+	}
+	for (int i = 1; i < world_size; i++)
+	{
+		printf("Message from Rank %d: %s", i, responses[i]);
+	}
+
+	printf("Rang %d beendet jetzt!\n", world_rank);
     // Finalize the MPI environment.
     MPI_Finalize();
 }
