3074526885961bda7714870cac7026c14b874ff0
Mytlogos <mytlogos@hotmail.de>
2019-11-19 20:15:07 +0100

    finish exercise 1

--- Blatt6/timempi.c
+++ Blatt6/timempi.c
@@ -1,7 +1,6 @@
+#define _DEFAULT_SOURCE
+
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-#include <inttypes.h>
 #include <string.h>
 #include <mpi.h>
 #include <unistd.h>
@@ -15,44 +14,40 @@ int main(int argc, char** argv) {
     // Get the number of processes
     int world_size;
     MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-	
+    
     // Get the rank of the process
     int world_rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
-		
-	char buffer[30];
-	struct timeval tv;
+        
+    char buffer[30];
+    struct timeval tv;
 
-	time_t curtime;
+    time_t curtime;
 
-	gettimeofday(&tv, NULL); 
-	curtime=tv.tv_sec;
+    gettimeofday(&tv, NULL); 
+    curtime=tv.tv_sec;
 
-	strftime(buffer,30,"%d.%m.%Y %T.",localtime(&curtime));
-	char name[150];
-	memset(name, 0, 150);
-	gethostname(name, 150);
+    // format time_val struct into a c string and put it into buffer
+    strftime(buffer,30,"%d.%m.%Y %T.",localtime(&curtime));
+    char name[150];
+
+    gethostname(name, 150);
     
-	char *responses[world_size];
+    if (world_rank == 0) {
+        for (int i = 1; i < world_size; i++){
+            char message[255];
+            // wait for message of process of rank i with a maximum of 255 chars (mpi_bytes)
+            MPI_Recv(message, 255, MPI_BYTE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            printf("%s\n", message);
+        }
+    } else {
+        char message[255];
+        sprintf(message, "%s %d: %s%ld", name, world_rank, buffer, tv.tv_usec);
+        MPI_Send(message, 255, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
+    }
 
-	char message[255];
-	sprintf(message, "%s: %s%ld\n", name, buffer, tv.tv_usec);
-
-	if (world_rank == 0) {
-		for (int i = 1; i < world_size; i++){
-			MPI_Recv(&responses[i], 255, MPI_BYTE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		}
-	} else {
-		char message[255];
-		sprintf(message, "%s: %s%ld\n", name, buffer, tv.tv_usec);
-		MPI_Send(message, 255, MPI_BYTE, 0, 0, MPI_COMM_WORLD);
-	}
-	for (int i = 1; i < world_size; i++)
-	{
-		printf("Message from Rank %d: %s", i, responses[i]);
-	}
-
-	printf("Rang %d beendet jetzt!\n", world_rank);
+    MPI_Barrier(MPI_COMM_WORLD);
+    printf("Rang %d beendet jetzt!\n", world_rank);
     // Finalize the MPI environment.
     MPI_Finalize();
 }
